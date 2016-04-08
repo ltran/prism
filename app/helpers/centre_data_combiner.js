2720e73928b548d2bea15174973fbc14d8b8de71
Louis Tran <tran.louis@gmail.com>
2016-04-07 17:18:15 -0700

    modify payload to use data and includes

--- app/helpers/centre_data_combiner.js
+++ app/helpers/centre_data_combiner.js
@@ -1,14 +1,33 @@
+var _ = require('underscore')
 /* eslint-disable camelcase */
 
 module.exports = function combineResults (results) {
-  var returnedData = []
+  var returnedData = {
+    data: [],
+    includes: []
+  }
+
   var categoriesData = results.categoriesData
   var centreData = results.centreData
 
-  results.storesData.forEach(function(store) {
-    store.centre = centreData.name
-    store.categories = categoriesData[store.store_id]
-    returnedData.push(store)
+  results.storesData.forEach(function (store) {
+    returnedData.data.push(store)
+  })
+
+  _.each(categoriesData, function (item) {
+    if (item.length > 0) {
+      returnedData.includes.push({
+        type: 'categories',
+        category_id: item[0].id,
+        name: item[0].name
+      })
+    }
+  })
+
+  returnedData.includes.push({
+    type: 'centre',
+    centre_id: centreData.centre_id,
+    name: centreData.name
   })
 
   return returnedData
